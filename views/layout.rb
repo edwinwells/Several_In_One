9fc7367ebce332a533833b02aa02106e51868d3f
edwinwells <edwin.wallace@yandex.com>
2017-02-28 15:36:15 -0500

    Initial stages of
    Three_In_One, a website to access, thru' user-selection, any of the three previous assignments, :
    ISBN_Refactored;
    Exact_Change: and/or
    New_Personal_Details.

---
+++ views/layout.rb
@@ -0,0 +1,12 @@
+<html>
+	<head>
+	  <title> 3 In 1 Website - Welcome!! </title>
+	</head>
+		<body class= "bgsilver">
+		
+	      <h1><a href = "http://C:/Users/edwin/mystuff/ISBN_Refactored/app.rb"> ISBN Validator </a>
+	      </h1>
+
+<%=yield%>
+		</body>
+</html>
