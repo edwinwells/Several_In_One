1cbfa1369caab743dd60c6474afbf9a248a5d90c
edwinwells <edwin.wallace@yandex.com>
2017-03-08 16:39:22 -0500

    Ok, feelin' good...
    added all required .erb's for the three programs.
    Added three href's to initial_page.erb.
    Populated app.rb with necessary get's and posts.
    Added reset button.
    Program functions beautifully, all that would remain is to improve ascetics.

--- app.rb
+++ app.rb
@@ -1,12 +1,107 @@
 require 'sinatra'
-# require_relative 'isbn_refactored.rb'
-# require_relative 'exact_change.rb'
+require_relative 'isbn_refactored.rb'
+require_relative 'exact_change.rb'
 # require_relative 'new_personal_details.rb'
 
 get '/' do
     erb :initial_page
 end
 
-post '/' do
-    erb :initial_page
+get '/isbn' do
+    erb :User_Input_ISBN_Number
 end
+
+get '/change' do
+    erb :User_Input_Amount
+end
+
+get '/details' do
+    erb :name
+end
+
+post '/name' do
+    name = params[:name]
+    redirect '/age?name=' + name
+end
+
+
+get '/age' do
+    erb :age, :locals => {:user_name => params[:user_name]}
+end
+
+post '/age' do
+    age = params[:age]
+    user_name = params[:user_name]
+    redirect '/location?user_name=' + user_name + '&age=' + age
+end
+
+get '/location' do
+    erb :location, :locals=>{:user_name=>params[:user_name],:age=>params[:age]}
+end
+
+post '/location' do
+    user_location = params[:user_location]
+    age = params[:age]
+    user_name = params[:user_name]
+    redirect '/numbers?user_name=' + user_name + '&age=' + age + '&user_location=' + user_location
+end
+
+get '/numbers' do
+    erb :numbers, :locals=>{:user_name=>params[:user_name],:age=>params[:age],:user_location=>params[:user_location]}
+end
+post '/numbers' do
+    num1 = params[:num1]
+    num2 = params[:num2]
+    num3 = params[:num3]
+    user_location = params[:user_location]
+    age = params[:age]
+    user_name = params[:user_name]
+
+    redirect '/favorites?num1=' + num1 + '&num2=' + num2 + '&num3=' + num3 + '&user_name=' + user_name + '&age=' + age + '&user_location=' + user_location
+end
+
+get '/favorites' do
+    num1 = params[:num1]
+    num2 = params[:num2]
+    num3 = params[:num3]
+    user_location = params[:user_location]
+    age = params[:age]
+    user_name = params[:user_name]
+    # sum_of_nums = params [:num1] + params[:num2] + params[:num3]
+
+    sum_of_nums = num1.to_i + num2.to_i + num3.to_i
+
+    if sum_of_nums > age.to_i
+        age_compare = "Your favorite numbers added together are greater than your age!"
+
+    elsif sum_of_nums < age.to_i
+        age_compare = "Your favorite numbers added together are less than your age!"
+
+    else
+        age_compare = "Your favorite numbers added together are equal to your age!"
+    end
+        erb :favorites, :locals=>{:user_name=>params[:user_name], :age=>params[:age],:user_location=>params[:user_location],:num1=>params[:num1],:num2=>params[:num2],:num3=>params[:num3],:age_compare=>age_compare,:sum_of_nums=>sum_of_nums}
+end
+
+post '/remove_unwanted(isbn_string)' do
+	number = params[:isbn_string]
+	result = remove_unwanted(number)
+
+	if result==true 
+		statement="\nCongratulations!\nYour ISBN #{number} is Valid!\nHave a nice day:-)\n\n"
+	else
+		statement="\nSorry 'bout your luck!\nYou got hold of a counterfeit ISBN!\nBetter luck next time...\n\n"
+	end
+end
+
+post '/exact_change(changevalue)' do
+	number = params[:changevalue]
+	result = exact_change(number)
+	# "OK, for #{number}, you will need #{result}!  Get to countin'!!"
+	erb :showinfo, :locals => {:number => number, 
+		                       :result => result}
+end
+
+# post '/' do
+#     erb :initial_page
+# end
