e373ba085012d9b47aeef1c68301025384bbe3ee
edwinwells <edwin.wallace@yandex.com>
2017-03-09 13:07:48 -0500

    Added completed URL
    to app.rb, Line 2.  Program functions; must modify for other two programs.

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require_relative 'isbn_refactored.rb'
+require_relative 'C:/Users/edwin/mystuff/ISBN_Refactored/isbn_refactored.rb'
 require_relative 'exact_change.rb'
 # require_relative 'new_personal_details.rb'
 
