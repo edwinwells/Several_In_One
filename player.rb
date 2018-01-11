1c648fc2f9805eb2ee620cb6c3d5f8c898be2a37
edwinwells <edwin.wallace@yandex.com>
2018-01-11 13:32:21 -0500

    Revised HumanPlayerX vs. UnbeatableComputerO, when move order is X=b, O=a1, X=a3, O=c1, X=b1, to block at b3 (when 4.)

--- player.rb
+++ player.rb
@@ -286,7 +286,7 @@ class ComputerUnbeatable < Player
    				    newgame.setup >= {a1: "O", b2: "X", c3: "X", c1: "O", a2: "X"} ||
    				    newgame.setup >= {a1: "O", b2: "X", c3: "X", c1: "O", a3: "X"} ||
    				    newgame.setup >= {a1: "O", b2: "X", c3: "X", c1: "O", b3: "X"} ||
-   				    newgame.setup >= {a1: "O", b2: "X", c3: "X", c1: "O", c2: "X"} ||
+                   newgame.setup >= {a1: "O", b2: "X", c3: "X", c1: "O", b3: "X"} ||
 				       newgame.setup >= {c2: "X", b2: "O", c1: "X", c3: "O", a1: "X"} ||
 				   	 newgame.setup >= {c1: "X", b2: "O", b3: "X", c3: "O", a1: "X"} ||
 				   	 newgame.setup >= {c1: "X", b2: "O", c3: "X", c2: "O", a2: "X"} ||
@@ -352,6 +352,7 @@ class ComputerUnbeatable < Player
    				    newgame.setup >= {c3: "X", b2: "O", c2: "X", c1: "O", a3: "X"} ||
    					 newgame.setup >= {a1: "X", b2: "O", c1: "X", b1: "O", a3: "X"} ||
                    newgame.setup >= {a1: "X", b2: "O", c1: "X", b1: "O", c3: "X"} ||
+                   newgame.setup >= {a1: "O", b2: "X", a3: "X", c1: "O", b1: "X"} ||
 					    newgame.setup >= {a1: "X", b2: "O", c1: "X", b1: "O", c2: "X"} ||
 					    newgame.setup >= {a1: "X", b2: "O", c1: "X", b1: "O", a2: "X"}	    
    					"b3"
